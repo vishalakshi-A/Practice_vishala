909d33d583492b482ac510ede755ec1704b2211c
Vishalakshi_A <visali1203@gmail.com>
2022-08-02 16:29:51 +0530

    done

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,19 +2,41 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+//    WebDriver driver;
+//    @BeforeAll
+//    public void setUp() {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://www.gmail.com/");
+//            }
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void login()
     {
-        assertTrue( true );
-    }
+        //driver.get("www.gmail.com");
+        //WebElement emailId=driver.findElement(By.id("identifierId"));
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.gmail.com/");
+        WebElement emailId=driver.findElement(By.id("identifierId"));
+
+       // WebElement emailId=driver.findElement(By.xpath("//*[@id='identifierId']"));
+
+        emailId.sendKeys("visali1203@gmail.com");
+         }
 }
